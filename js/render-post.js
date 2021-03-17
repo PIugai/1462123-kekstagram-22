b0374fd9b638d80077d4557d9b957ef2ce77a156
Pavel Iugai <pavel.iugai@outlook.com>
2021-03-17 11:28:00 +0800

    Fix render issue

--- js/render-post.js
+++ js/render-post.js
@@ -7,15 +7,14 @@ const similarListElement = document.querySelector('.pictures');
 const similarPostTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
 const similarPosts = renderPost(POST_AMOUNT);
-console.log(similarPosts)
 
 const renderSimilarPost = () => {
   const similarListFragment = document.createDocumentFragment();
 
   similarPosts.forEach(({url, desc, likes}) => {
     const postElement = similarPostTemplate.cloneNode(true);
-    postElement.querySelector('.picture__img')["src"] = url;
-    postElement.querySelector('.picture__img')["alt"] = desc;
+    postElement.querySelector('.picture__img')['src'] = url;
+    postElement.querySelector('.picture__img')['alt'] = desc;
     postElement.querySelector('.picture__likes').innerText = likes;
     similarListFragment.appendChild(postElement);
   });
