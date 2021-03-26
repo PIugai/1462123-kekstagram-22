87c364184928b10622cf7000688f1e638183c956
Pavel Iugai <pavel.iugai@outlook.com>
2021-03-26 16:37:09 +0800

    remove else

--- js/api.js
+++
@@ -1,9 +0,0 @@
-const getData = (onSuccess) => {
-  fetch('https://22.javascript.pages.academy/code-and-magick/data')
-    .then((response) => response.json())
-    .then((wizards) => {
-      onSuccess(wizards);
-    });
-};
-
-export { getData }
--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
-import { getData } from './api.js'
 import { renderPosts } from './render-posts.js';
 import { mockData } from './generate-posts.js';
 import './image-upload.js';
 import './editor.js';
 
-// renderPosts(mockData);
-
-getData((posts) => {
-  renderPosts(posts);
-});
+renderPosts(mockData);
